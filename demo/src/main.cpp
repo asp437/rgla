fc0fe49fd021854e9da89f46b8573f8414560d21
Aleksandr Parfenov <asp437@gmail.com>
2021-02-23 13:47:30 +0300

    Move demo application into separate build target

--- demo/src/main.cpp
+++ demo/src/main.cpp
@@ -1,12 +1,12 @@
 #include <logging.hpp>
-#include <rgla_config.h>
+// #include <rgla_config.h>
 #include <application.hpp>
 #include <rgla_application.hpp>
 #include <vertex.hpp>
 
 int main() {
     RGLA::Logger::InitLogger(std::cerr, RGLA::Logger::LoggerLevel::LOG_INFO);
-    RGLA::Logger::LogDebug() << "OpenGL Playground Version " << rgla_VERSION_MAJOR << "." << rgla_VERSION_MINOR << std::endl;
+    // RGLA::Logger::LogDebug() << "OpenGL Playground Version " << rgla_VERSION_MAJOR << "." << rgla_VERSION_MINOR << std::endl;
     RGLA::Logger::LogDebug() << "Size of Vertex: " << sizeof(RGLA::Vertex) << std::endl;
     try {
         RGLA::Logger::LogInfo() << "Starting..." << std::endl;
@@ -15,7 +15,9 @@ int main() {
         app.MainLoop();
         RGLA::Logger::LogInfo() << "Exiting" << std::endl;
     } catch (const std::exception& e) {
-        RGLA::Logger::LogError() << "Exception: " << e.what() << std::endl;
+        RGLA::Logger::LogError() << "Caught exception: " << e.what() << std::endl;
+        RGLA::Logger::LogError() << "Exit on failure" << std::endl;
+        return 1;
     }
     return 0;
 }
