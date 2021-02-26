800d925745838890fee474e5acbb05e01705c69d
Aleksandr Parfenov <asp437@gmail.com>
2021-02-27 01:26:30 +0300

    Added dynamic in-memory textures

--- include/texture.hpp
+++ include/texture.hpp
@@ -7,16 +7,37 @@ namespace RGLA {
     // Also support creation of image without file
     class Texture {
     public:
-        Texture(const std::string& fileName, bool flipVertical = false);
         virtual ~Texture();
 
         void Use(uint textureUnit) const;
+    protected:
+        Texture() = default;
+        void BindData();
+        void UpdateData();
 
-    private:
         uint _id = 0;
         int _width;
         int _height;
         int _channels;
-        unsigned char* _stbData = nullptr;
+        unsigned char* _data = nullptr;
+    };
+
+    class FileTexture : public Texture {
+    public:
+        FileTexture(const std::string& fileName, bool flipVertical = false);
+        ~FileTexture();
+    private:
+    };
+
+    class InMemoryTexture : public Texture {
+    public:
+        using Pixel = uint32;
+
+        InMemoryTexture(const int width, const int height, const int channels);
+        ~InMemoryTexture();
+
+        Pixel GetPixel(int x, int y) const;
+        void SetPixel(int x, int y, Pixel color);
+    private:
     };
 }
--- src/texture.cpp
+++ src/texture.cpp
@@ -6,35 +6,84 @@
 #include <stb_image.h>
 
 namespace RGLA {
-    Texture::Texture(const std::string& fileName, bool flipVertical) {
+    FileTexture::FileTexture(const std::string& fileName, bool flipVertical) {
         stbi_set_flip_vertically_on_load(flipVertical);
-        _stbData = stbi_load(fileName.c_str(), &_width, &_height, &_channels, 0);
-        if (!_stbData) {
+        _data = stbi_load(fileName.c_str(), &_width, &_height, &_channels, 0);
+        if (!_data) {
             throw std::runtime_error("Can't load image " + fileName);
         }
-        glGenTextures(1, &_id);
-        glBindTexture(GL_TEXTURE_2D, _id);
-        // TODO: Dynamic source image format detection
-        if (_channels == 4) {
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, _width, _height, 0, GL_RGBA, GL_UNSIGNED_BYTE, _stbData);
-        } else {
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, _width, _height, 0, GL_RGB, GL_UNSIGNED_BYTE, _stbData);
+        BindData();
+    }
+
+    InMemoryTexture::InMemoryTexture(const int width, const int height, const int channels) {
+        RGLA_ENSURE(channels == 4, "InMemoryTexture can be only with 4 channels");
+        _width = width;
+        _height = height;
+        _channels = channels;
+        _data = (unsigned char*) malloc(_width * _height * _channels);
+        memset(_data, 0, _width * _height * _channels);
+        BindData();
+    }
+
+    InMemoryTexture::~InMemoryTexture() {
+        free(_data);
+    }
+
+    FileTexture::~FileTexture() {
+        if (_data) {
+            stbi_image_free(_data);
+            _data = nullptr;
         }
-        glGenerateMipmap(GL_TEXTURE_2D);
     }
 
     Texture::~Texture() {
-        if (_stbData) {
-            stbi_image_free(_stbData);
-            _stbData = nullptr;
-        }
         if (_id) {
             glDeleteTextures(1, &_id);
         }
     }
 
+    void Texture::BindData() {
+        RGLA_ENSURE(_data, "Texture data is null");
+        glGenTextures(1, &_id);
+        glBindTexture(GL_TEXTURE_2D, _id);
+        UpdateData();
+    }
+
+    void Texture::UpdateData() {
+        // TODO: Dynamic source image format detection
+        if (_channels == 4) {
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, _width, _height, 0, GL_RGBA, GL_UNSIGNED_BYTE, _data);
+        } else {
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, _width, _height, 0, GL_RGB, GL_UNSIGNED_BYTE, _data);
+        }
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+
     void Texture::Use(uint textureUnit) const {
         glActiveTexture(GL_TEXTURE0 + textureUnit);
         glBindTexture(GL_TEXTURE_2D, _id);
+    }
+
+    InMemoryTexture::Pixel InMemoryTexture::GetPixel(int x, int y) const {
+        if (x < 0 || x >= _width || y < 0 || y >= _height) {
+            throw std::runtime_error("Try to get pixel out of bounds of the texture");
+        }
+        Pixel result = 0;
+        for (int i = 0; i < 4; i++) {
+            result |= _data[(x + y * _width) * _channels + i];
+            result <<= 8;
+        }
+        return result;
+    }
+
+    void InMemoryTexture::SetPixel(int x, int y, Pixel color) {
+        if (x < 0 || x >= _width || y < 0 || y >= _height) {
+            throw std::runtime_error("Try to set pixel out of bounds of the texture");
+        }
+        for (int i = 0; i < 4; i++) {
+            _data[(x + y * _width) * _channels + i] = color & 0xFF;
+            color >>= 8;
+        }
+        UpdateData();
     }
 }
